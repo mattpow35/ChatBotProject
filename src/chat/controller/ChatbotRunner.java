a691541818b285d94f53f64ab27170ca96cfc8c4
Matt Powley <mattpow35@gmail.com>
2016-10-14 08:42:20 -0600

    added runner method

--- src/chat/controller/ChatbotRunner.java
+++ src/chat/controller/ChatbotRunner.java
@@ -2,5 +2,10 @@ package chat.controller;
 
 public class ChatbotRunner
 {
-
+	public static void main (String [] args)
+	{
+		ChatbotController chatbotAppController = new ChatbotController();
+		chatbotAppController.start();
+		
+	}
 }
