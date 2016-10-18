dc23b8c82ca5c6189e24f3cdcd4ced1391d0be6c
mattpow35 <mpow2231@740s-pl-2.local>
2016-10-18 08:56:02 -0600

    created a loop in the controller class. repeating until length checker failed

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,9 +1,27 @@
 package chat.controller;
 
+import chat.model.Chatbot;
+import chat.view.ChatViewer;
+
 public class ChatbotController
 {
+	private Chatbot stupidBot;
+	private ChatViewer display;
+	
+	public ChatbotController()
+	{
+		stupidBot = new Chatbot("stupid robot");
+		display = new ChatViewer();
+	}
+	
+	
 	public void start()
 	{
+		String response = "words";
 		
+		while(stupidBot.lengthChecker(response))
+		{
+			response = display.collectResponse("What do you want to talk about????");
+		}
 	}
 }
