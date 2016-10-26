e3ffad045edf8566879e4a08737777bdbd035e97
mattpow35 <mpow2231@740s-pl-2.local>
2016-10-26 08:35:14 -0600

    finished adding icon completely

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -28,7 +28,7 @@ public class ChatbotController
 	
 	private String useChatbotCheckers(String input)
 	{
-		String checkedInput = "I have no idea what you mean about " + input;
+		String checkedInput = "";
 		
 		if (stupidBot.memeChecker(input))
 		{
@@ -37,6 +37,10 @@ public class ChatbotController
 		if (stupidBot.contentChecker(input))
 		{
 			checkedInput += "\nYou know my secret topic!\n";
+		}
+		if (checkedInput.length() == 0)
+		{
+			checkedInput = "I have no idea what you mean about " + input;
 		}
 		
 		return checkedInput;
