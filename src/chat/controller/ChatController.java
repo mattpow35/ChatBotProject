56fa48b0656b25d43c359aaf6c1a670730ac1d2e
mattpow35 <mpow2231@740s-pl-2.local>
2016-11-22 08:56:49 -0700

    added the quit method for quit checker

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -20,51 +20,60 @@ public class ChatController
 	
 	public void start()
 	{
-//		String response = display.collectResponse("What do you want to talk about????");
-//		
-//		while(stupidBot.lengthChecker(response))
-//		{
-//			display.collectResponse("Oh you want to talk bout " + response +"? Tell me more...");
-//			display.displayMessage(useChatbotCheckers(response));
-//			
-//		}
+		
+
 	}
 	
 	public String useChatbotCheckers(String input)
 	{
 		String checkedInput = "";
+		if(!stupidBot.quitChecker(input))
+		{
 		
-		if (stupidBot.memeChecker(input))
-		{
-			checkedInput += "\nYou like dank memes!\n ";
+			if (stupidBot.memeChecker(input))
+			{
+				checkedInput += "\nYou like dank memes!\n ";
+			}
+			if (stupidBot.contentChecker(input))
+			{
+				checkedInput += "\nYou know my secret topic!\n";
+			}
+			if (checkedInput.length() == 0)
+			{
+				checkedInput = "I have no idea what you mean about " + input;
+			}
+			if (stupidBot.politicalTopicChecker(input))
+			{
+				checkedInput += "\nYou want to talk about politics.\n";
+			}
+			if (stupidBot.keyboardMashChecker(input))
+			{
+				checkedInput += "\nYou just mashed random things on the keyboard\n";
+			}
+			if (stupidBot.twitterChecker(input))
+			{
+				checkedInput +="\nYou want to talk about twitter\n";
+			}
+			if (stupidBot.inputHTMLChecker(input))
+			{
+				checkedInput += "\nYou want to talk about HTML\n";
+			}
+			if (stupidBot.quitChecker(input))
+			{
+				checkedInput += "\nYou want to quit the chatbot :(\n";
+			}
+			
+			int canBeRandom= (int) (Math.random() *2);
+			if(canBeRandom % 2 == 0)
+			{
+				checkedInput += randomTopicGenerator();
+			}
+		
 		}
-		if (stupidBot.contentChecker(input))
+		else
 		{
-			checkedInput += "\nYou know my secret topic!\n";
-		}
-		if (checkedInput.length() == 0)
-		{
-			checkedInput = "I have no idea what you mean about " + input;
-		}
-		if (stupidBot.politicalTopicChecker(input))
-		{
-			checkedInput += "\nYou want to talk about politics.\n";
-		}
-		if (stupidBot.keyboardMashChecker(input))
-		{
-			checkedInput += "\nYou just mashed random things on the keyboard\n";
-		}
-		if (stupidBot.twitterChecker(input))
-		{
-			checkedInput +="\nYou want to talk about twitter\n";
-		}
-		if (stupidBot.inputHTMLChecker(input))
-		{
-			checkedInput += "\nYou want to talk about HTML\n";
-		}
-		if (stupidBot.quitChecker(input))
-		{
-			checkedInput += "\nYou want to quit the chatbot :(\n";
+			display.displayMessage("Thanks for chatting, seeya soon!");
+			System.exit(0);
 		}
 		
 		return checkedInput;
